1cb8ebb67a9de13a6cb19f0d2e36ffbd2035e3f4
malw4re <135704868+br4nch@users.noreply.github.com>
2024-09-01 16:36:31 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+  name="luma-api",
+  version="1.0.0",
+  url="https://github.com/br4nch/luma.api",
+  description="Python wrapper for luma api",
+  author="stand",
+  python_requires=">=3.9.0",
+  install_requires=["aiohttp"]
+)
