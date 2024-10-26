6699a6343e238bd98958eb1e6e3c7577a4d85c89
malw4re <135704868+br4nch@users.noreply.github.com>
2024-10-26 11:15:36 +0300

    typo

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-  name="luma-api",
+  name="luma_api",
   version="1.0.0",
   url="https://github.com/br4nch/luma.api",
-  description="Python wrapper for luma api",
+  description="A python wrapper for luma api",
   author="stand",
   python_requires=">=3.9.0",
   install_requires=["aiohttp"]
