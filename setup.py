55148522ae6dba90504a7aaec8f41d46cc6ae6f2
malw4re <135704868+br4nch@users.noreply.github.com>
2024-10-26 12:07:26 +0300

    typo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name="fulcrum_api",
-  version="0.0.2",
+  version="0.0.3",
   url="https://github.com/br4nch/fulcrum_api",
   description="A python wrapper for fulcrum api",
   author="br4nch",
