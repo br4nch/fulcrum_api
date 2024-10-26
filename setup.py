0e5d21c75a5de9eb49677c6c641c0e405c6ece6f
malw4re <135704868+br4nch@users.noreply.github.com>
2024-10-26 11:42:06 +0300

    typo

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-  name="luma_api",
+  name="fulcrum_api",
   version="0.0.2",
-  url="https://github.com/br4nch/luma.api",
-  description="A python wrapper for luma api",
-  author="stand",
+  url="https://github.com/br4nch/fulcrum_api",
+  description="A python wrapper for fulcrum api",
+  author="br4nch",
   python_requires=">=3.9.0",
   install_requires=["aiohttp"]
 )
