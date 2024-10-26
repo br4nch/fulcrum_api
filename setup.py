efbec3b0b694652fed834f74dd522c286d6459ea
malw4re <135704868+br4nch@users.noreply.github.com>
2024-10-26 11:21:11 +0300

    typo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name="luma_api",
-  version="1.0.0",
+  version="0.0.2",
   url="https://github.com/br4nch/luma.api",
   description="A python wrapper for luma api",
   author="stand",
